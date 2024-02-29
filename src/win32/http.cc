2dd09eeb2bc0ffb7574abe0e0b68d2138184ae87
Darshan Sen <raisinten@gmail.com>
2024-02-29 13:38:10 +0530

    Call callback in Windows impl
    Signed-off-by: Darshan Sen <raisinten@gmail.com>

--- src/win32/http.cc
+++ src/win32/http.cc
@@ -2,12 +2,19 @@
 
 #include <map>     // std::map
 #include <string>  // std::string
+#include <thread>  // std::thread
 #include <variant> // std::variant
 
 namespace req {
 
 auto request(const std::string &url, RequestOptions options,
              std::function<void(std::variant<std::string, Response>)> callback)
-    -> void {}
+    -> void {
+  std::thread([callback]() {
+    std::variant<std::string, Response> result;
+    result = Response{"Hello, world!", 200, {}};
+    callback(std::move(result));
+  }).detach();
+}
 
 } // namespace req
