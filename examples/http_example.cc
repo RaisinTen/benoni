e6715d0bbaf491da7a725168ce82a99997fd54d4
Darshan Sen <raisinten@gmail.com>
2024-03-06 15:37:39 +0530

    Use callback approach in example
    Signed-off-by: Darshan Sen <raisinten@gmail.com>

--- examples/http_example.cc
+++ examples/http_example.cc
@@ -1,6 +1,5 @@
 #include <http.h>
 
-#include <future>
 #include <iostream>
 #include <variant>
 
@@ -10,43 +9,33 @@ using req::RequestOptions;
 using req::RequestOptionsBuilder;
 using req::Response;
 
-auto request_promisified(
-    const std::string &url,
-    RequestOptions options = RequestOptionsBuilder().build())
-    -> std::future<Response> {
-  std::shared_ptr<std::promise<Response>> promise =
-      std::make_shared<std::promise<Response>>();
-
-  std::future<Response> future = promise->get_future();
-
-  request(url, options, [promise](std::variant<std::string, Response> result) {
-    // This callback is run in a background thread.
-    if (std::holds_alternative<std::string>(result)) {
-      promise->set_exception(std::make_exception_ptr(
-          std::runtime_error(std::get<std::string>(result))));
-      return;
-    }
-    promise->set_value(std::get<Response>(result));
-  });
-
-  return future;
-}
-
 int main() {
   std::cout << "before request" << std::endl;
-  try {
-    std::future<Response> response_future =
-        request_promisified("https://postman-echo.com/get");
-    Response response = response_future.get();
 
-    std::cout << "after request" << std::endl;
+  request("https://postman-echo.com/get", RequestOptionsBuilder{}.build(),
+          [](std::variant<std::string, Response> result) {
+            std::cout << "inside callback" << std::endl;
 
-    std::cout << "response body: [" << response.body << "]" << std::endl;
-    std::cout << "response status: [" << response.status << "]" << std::endl;
-  } catch (const std::exception &exception) {
-    std::cerr << "error: [" << exception.what() << "]" << std::endl;
-    return 1;
-  }
+            // This callback is run in a background thread.
+            if (std::holds_alternative<std::string>(result)) {
+              std::cerr << "error: [" << std::get<std::string>(result) << "]"
+                        << std::endl;
+              exit(EXIT_FAILURE);
+            }
+
+            Response response{std::get<Response>(result)};
+
+            std::cout << "response body: [" << response.body << "]"
+                      << std::endl;
+            std::cout << "response status: [" << response.status << "]"
+                      << std::endl;
+            exit(EXIT_SUCCESS);
+          });
+
+  std::cout << "after request" << std::endl;
+
+  while (true)
+    ;
 
   return 0;
 }
